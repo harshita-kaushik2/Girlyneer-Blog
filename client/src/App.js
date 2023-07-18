588a5952d078901522f02f64492b2621ad75066a
Harshita Kaushik <harshitakaushik.hk@gmail.com>
2023-07-18 19:03:56 +0530

    Revert "react-router-dom"
    This reverts commit 5a3bf94cb4f4926a018050b7bd9e98c93e796d9f.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,57 @@
 import './App.css';
-import Post from "./Post";
-import Header from "./Header";
-import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-  <Routes>
-    <Route index element={
-      <main>
-        <Header />
-        <Post />
-        <Post />
-        <Post />
-      </main>
-    } />
-  </Routes>
+  <main>
+    <header>
+      <a href="" className="logo">Girlyneer</a>
+      <nav>
+      <a href="">Login</a>
+      <a href="">Register</a>
+      </nav>
+    </header>
+    <div className="post">
+      <div className="image">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8V2thbJQ8aL-ZkwQeMYje0ZrkLH1jQTW3mw"></img>
+      </div>
+      <div className="texts">
+      <h2>Life of Girls at NIT Agartala</h2>
+      <p className="info">
+        <a className="author">Harshita Kaushik</a>
+        <time>17-07-2023 20:07</time>
+      </p>
+      <p className="summary">Consectetur nisi dolor exercitation eu cupidatat mollit ut sit. Sint pariatur ex Ex qui consectetur duis id proident sunt irure cillum minim. Amet labore do aute esse. Enim veniam minim reprehenderit nostrud enim excepteur et incididunt ex fugiat ut. Voluptate consectetur irure officia nulla labore esse dolore aute incididunt.</p>
+      </div> 
+    </div>
+    <div className="post">
+      <div className="image">
+      <img src="https://tripuratimes.com/backoffice/NewsImages/6769.jpg"></img>
+      </div>
+      <div className="texts">
+      <h2>Life of Girls at NIT Agartala</h2>
+      <p className="info">
+        <a className="author">Harshita Kaushik</a>
+        <time>17-07-2023 20:07</time>
+      </p>
+      <p className="summary">Consectetur nisi dolor exercitation eu cupidatat mollit ut sit. Sint pariatur eu veniam irure et nulla duis ad. Ex qui consectetur duis id proident sunt irure cillum minim. Amet labore do aute esse. Enim veniam minim reprehenderit nostrud enim excepteur et incididunt ex fugiat ut. Voluptate consectetur irure officia nulla labore esse dolore aute incididunt.</p>
+      </div>
+    </div>
+    <div className="post">
+      <div className="image">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8V2thbJQ8aL-ZkwQeMYje0ZrkLH1jQTW3mw"></img>
+      </div>
+      <div className="texts">
+      <h2>Life of Girls at NIT Agartala</h2>
+      <p className="info">
+        <a className="author">Harshita Kaushik</a>
+        <time>17-07-2023 20:07</time>
+      </p>
+      <p className="summary">Consectetur nisi dolor exercitation eu cupidatat mollit ut sit. Sint pariatur ex la qui consectetur duis id proident sunt irure cillum minim. Amet labore do aute esse. Enim veniam minim reprehenderit nostrud enim excepteur et incididunt ex fugiat ut. Voluptate consectetur irure officia nulla labore esse dolore aute incididunt.</p>
+      </div>
+    </div>
+    
+    
+  </main> 
   );
 }
 
